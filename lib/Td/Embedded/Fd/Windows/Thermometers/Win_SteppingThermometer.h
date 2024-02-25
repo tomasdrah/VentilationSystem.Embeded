f5d1267e7ea959900fb700672d97bbd52a977cd4
drahonovsky <drahonovsky@nextis.cz>
2024-02-25 16:33:33 +0100

    added stepping thermometer for testing

---
+++ lib/Td/Embedded/Fd/Windows/Thermometers/Win_SteppingThermometer.h
@@ -0,0 +1,58 @@
+//
+// Created by drahonovsky on 25.02.2024.
+//
+
+#ifndef VENTILATIONSYSTEM_EMBEDDED_WIN_STEPPINGTHERMOMETER_H
+#define VENTILATIONSYSTEM_EMBEDDED_WIN_STEPPINGTHERMOMETER_H
+
+#include "Embedded/Fd/Headers/Thermometers/Thermometer.h"
+
+namespace TdEmbeddedFd {
+
+    class Win_SteppingThermometer : Thermometer {
+    public:
+        explicit Win_SteppingThermometer(
+                float currentTemp=40,
+                float minTemp=30,
+                float maxTemp=50,
+                float stepSize=1,
+                bool direction= true) : currentTemp(currentTemp),
+                                  minTemp(minTemp),
+                                  maxTemp(maxTemp),
+                                  stepSize(stepSize),
+                                  direction(direction) {}
+
+        float ReadTemperatureCelsius() override {
+            if (direction) {
+                currentTemp += stepSize;
+                if (currentTemp >= maxTemp) {
+                    currentTemp = maxTemp;
+                    direction = false;  // Change direction
+                }
+            } else {
+                currentTemp -= stepSize;
+                if (currentTemp <= minTemp) {
+                    currentTemp = minTemp;
+                    direction = true;  // Change direction
+                }
+            }
+
+            return currentTemp;
+        }
+
+        bool TryToInitialize() override {
+            return true;
+        }
+
+    private:
+        float currentTemp;  // Current temperature
+        float minTemp;      // Minimum temperature
+        float maxTemp;      // Maximum temperature
+        float stepSize;     // Step size for temperature change
+        bool direction;     // Direction of temperature change (true for increasing, false for decreasing)
+
+    };
+
+} // TdEmbeddedFd
+
+#endif //VENTILATIONSYSTEM_EMBEDDED_WIN_STEPPINGTHERMOMETER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h>
+
+#include <Wire.h>
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BMP280.h"
+
+
+
 #include "Embedded/Fd/Windows/Win_IoPin.h"
 #include "Embedded/Fd/Arduino/Ard_IoPin.h"
+#include "Embedded/Fd/Windows/Thermometers/Win_SteppingThermometer.h"
 
-TdEmbeddedFd::Win_IoPin _Wpin = TdEmbeddedFd::Win_IoPin();
-TdEmbeddedFd::Ard_IoPin _Apin = TdEmbeddedFd::Ard_IoPin(LED_BUILTIN, OUTPUT );
-TdEmbeddedFd::Ard_IoPin _A_analog_pin = TdEmbeddedFd::Ard_IoPin(A0, INPUT );
+TdEmbeddedFd::Win_IoPin W_pin = TdEmbeddedFd::Win_IoPin();
+TdEmbeddedFd::Ard_IoPin A_pin = TdEmbeddedFd::Ard_IoPin(LED_BUILTIN, OUTPUT );
+TdEmbeddedFd::Ard_IoPin A_analog_pin = TdEmbeddedFd::Ard_IoPin(A0, INPUT );
+
+TdEmbeddedFd::Win_SteppingThermometer w_thermometer = TdEmbeddedFd::Win_SteppingThermometer();
 
 TdEmbeddedFd::PinState testGetState(TdEmbeddedFd::IoPin *pin) {
     return pin->getState();
@@ -15,13 +25,15 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
 
-    _Wpin.setState(TdEmbeddedFd::PinState::On);
-    Serial.println((testGetState(&_Wpin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
+    W_pin.setState(TdEmbeddedFd::PinState::On);
+    Serial.println((testGetState(&W_pin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
 
-    _Apin.setState(TdEmbeddedFd::PinState::On);
-    Serial.println((testGetState(&_Apin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
-    _Apin.setState(TdEmbeddedFd::PinState::Off);
-    Serial.println((testGetState(&_Apin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
+    A_pin.setState(TdEmbeddedFd::PinState::On);
+    Serial.println((testGetState(&A_pin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
+    A_pin.setState(TdEmbeddedFd::PinState::Off);
+    Serial.println((testGetState(&A_pin) == TdEmbeddedFd::PinState::On) ? "On" : "Off");
+
+    w_thermometer.TryToInitialize();
 }
 
 
@@ -29,12 +41,22 @@ void loop() {
     int sensorValue = 0;
 
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(2000);
-    sensorValue = _A_analog_pin.getSettPwm();
+    delay(1000);
+    sensorValue = A_analog_pin.getSettPwm();
     Serial.println(sensorValue);
+
+    Serial.print("Temperature: ");
+    Serial.print(w_thermometer.ReadTemperatureCelsius());
+    Serial.println(" celsius.");
+
 
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);
-    sensorValue = _A_analog_pin.getSettPwm();
+    sensorValue = A_analog_pin.getSettPwm();
     Serial.println(sensorValue);
+
+    Serial.print("Temperature: ");
+    Serial.print(w_thermometer.ReadTemperatureCelsius());
+    Serial.println(" celsius.");
+
 }
