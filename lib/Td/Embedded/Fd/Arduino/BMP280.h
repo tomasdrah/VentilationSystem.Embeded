fb09580ea2229610bb8923ea593181eef7f55633
drahonovsky <drahonovsky@nextis.cz>
2024-02-25 15:44:25 +0100

    working on BMP280

---
+++ lib/Td/Embedded/Fd/Arduino/BMP280.h
@@ -0,0 +1,20 @@
+//
+// Created by drahonovsky on 25.02.2024.
+//
+
+#ifndef VENTILATIONSYSTEM_EMBEDDED_BMP280_H
+#define VENTILATIONSYSTEM_EMBEDDED_BMP280_H
+
+namespace TdEmbeddedFd {
+
+    class BMP280 {
+    public:
+        float ReadTemperature(){
+            return 0.0;
+        }
+
+    };
+
+} // TdEmbeddedFd
+
+#endif //VENTILATIONSYSTEM_EMBEDDED_BMP280_H
